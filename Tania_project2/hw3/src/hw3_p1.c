2e00a53e4cdafe1272c88c6c4238ea64905b2006
Lovely Fellow <44167585+Lovely-Fellow@users.noreply.github.com>
2019-04-25 20:32:42 +0200

    Update hw3_p1.c

--- Tania_project2/hw3/src/hw3_p1.c
+++ Tania_project2/hw3/src/hw3_p1.c
@@ -12,27 +12,47 @@ int strComparator(void* str1, void* str2) {
 
 int idComparator(void *student1, void *student2){
 	//Insert Code here
-    return 2;
+	return intComparator((Student *)student1->id, (Student *) studnet2->id);
 }	
 
 int nameComparator(void *student1, void *student2){
 	//Insert Code here
-    return 2;
+	int result = strComparator((Student*)student1->name->lastNamme, (Student *)student2->name->lastName);
+	if ((!result)
+	{
+		int result = strComparator((Student*)student1->name->firstName, (Student *)student2->name->firstName);
+	}
+	if ((!result)
+	{
+		int result = strComparator((Student*)student1->name->middleName, (Student *)student2->name->middleName);
+	}
+    return result;
 }
 
 int totalHWComparator(void *student1, void *student2){
 	//Insert Code here
-    return 2;
+
+    return intComparator( (Student*)student1->hw_score[0] + (Student*)student1->hw_score[1] + (Student*)student1->hw_score[2],
+		(Student*)student2->hw_score[0] + (Student*)student2->hw_score[1] + (Student*)student2->hw_score[2]);
 }
 
 int totalMidtermComparator(void *student1, void *student2){
 	//Insert Code Here
-    return 2;
+    return intComparator( (Student*)student1->m_scores->m1_score + (Student*)student1->m_scores->m2_score,
+		(Student*)student2->m_scores->m1_score + (Student*)student2->m_scores->m2_score);
 }
 
 int totalPtsComparator(void *student1, void *student2){
 	//Insert Code Here
-    return 2;
+    return ((Student*)student1->hw_score[0] + (Student*)student1->hw_score[1] + (Student*)student1->hw_score[2] 
+		+ (Student*)student1->m_scores->m1_score + (Student*)student1->m_scores->m2_score,
+		(Student*)student2->hw_score[0] + (Student*)student2->hw_score[1] + (Student*)student2->hw_score[2] 
+		+ (Student*)student2->m_scores->m2_score + (Student*)student2->m_scores->m2_score));
+}
+
+voip printCSVStudent(List_t* list){
+	printList(list, STR_MODE);
+	printf("\n");
 }
 
 
